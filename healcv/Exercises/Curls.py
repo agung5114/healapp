f9c19a298d0aeef754ffdcc60d68c13d175d85f2
agung5114 <43102810+agung5114@users.noreply.github.com>
2022-05-26 11:26:05 +0700

    Update Curls.py

--- healcv/Exercises/Curls.py
+++ healcv/Exercises/Curls.py
@@ -27,7 +27,31 @@ WEBRTC_CLIENT_SETTINGS = ClientSettings(
     rtc_configuration={"iceServers": [{"urls": ["stun:stun.l.google.com:19302"]}]},
     media_stream_constraints={"video": True, "audio": False},
     )
-    
+ from streamlit.server.server import SessionInfo
+
+try:
+    from streamlit.scriptrunner import get_script_run_ctx
+except ModuleNotFoundError:
+    # streamlit < 1.8
+    try:
+        from streamlit.script_run_context import get_script_run_ctx  # type: ignore
+    except ModuleNotFoundError:
+        # streamlit < 1.4
+        from streamlit.report_thread import (  # type: ignore
+            get_report_ctx as get_script_run_ctx,
+        )
+
+from streamlit.server.server import Server
+
+# Ref: https://gist.github.com/tvst/036da038ab3e999a64497f42de966a92
+
+
+def get_session_id() -> str:
+    ctx = get_script_run_ctx()
+    if ctx is None:
+        raise Exception("Failed to get the thread context")
+
+    return ctx
 # define video transformer class 
 class VideoTransformer(VideoTransformerBase):
         frame_lock: threading.Lock  # `transform()` is running in another thread, then a lock object is used here for thread-safety.
