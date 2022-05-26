2c12b26190b05546998a67ad351c4b7eadc8cd0e
agung5114 <43102810+agung5114@users.noreply.github.com>
2022-05-26 12:47:23 +0700

    Update Curls.py

--- healcv/Exercises/Curls.py
+++ healcv/Exercises/Curls.py
@@ -36,10 +36,10 @@ import cv2
 class VideoProcessor:
     def recv(self, frame):
         img = frame.to_ndarray(format="bgr24")
+        return img
+#         img = cv2.cvtColor(cv2.Canny(img, 100, 200), cv2.COLOR_GRAY2BGR)
 
-        img = cv2.cvtColor(cv2.Canny(img, 100, 200), cv2.COLOR_GRAY2BGR)
-
-        return av.VideoFrame.from_ndarray(img, format="bgr24")
+#         return av.VideoFrame.from_ndarray(img, format="bgr24")
 
 # ctx = webrtc_streamer(key="example", client_settings=WEBRTC_CLIENT_SETTINGS,video_processor_factory=VideoProcessor)
     
